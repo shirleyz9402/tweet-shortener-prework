024d47e51df5f13a17e019fd97e55d3bf70b7c97
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-28 17:26:12 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,9 +31,18 @@ def bulk_tweet_shortener(tweets)
 end 
   
 def selective_tweet_shortener(tweet)
-  if tweet.length > 140
+  if tweet.size > 140
     word_substituter(tweet)
   else 
     tweet
   end
+end 
+
+def shortened_tweet_truncator(tweet)
+  subbed = selective_tweet_shortener(tweet)
+  if subbed.size > 140
+    puts "#{subbed[0..139]}..."
+  else 
+    subbed
+  end 
 end 
