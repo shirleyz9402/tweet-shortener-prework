53e3426363fc7ee5759509109a037cef57bf9a98
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-28 17:02:13 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -25,6 +25,8 @@ def  word_substituter(tweet)
 end 
 
 def bulk_tweet_shortener(tweets)
-  word_substituter(tweets.to_s)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end 
 end 
   
