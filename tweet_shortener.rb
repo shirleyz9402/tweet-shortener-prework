de3b86c7899d4c152c58dbc7940f6a498785a4b4
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-28 02:22:15 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,6 +1,13 @@
 def dictionary
   subs = {
-    :hi => [""]  
+    hi => "hello",
+    "2" => ["to", "two", "too"],
+    "4" => ["for", "four"],
+    b => "be",
+    u => "you"
+    @ => "at"
+    "&" => "and"
     
+        
   }
   
